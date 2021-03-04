66494665999bd7383288923ccfaf8fa6267a7164
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2021-03-04 17:26:53 +0100

    include socket fix aimed for previous commit

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
   experimentId: '174',
   submissionUrl: 'http://magpie-demo.herokuapp.com/api/submit_experiment/174',
-  socketUrl: 'magpie-demo.herokuapp.com/socket',
+  socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
   contactEmail: 'test@random.com'
 };
