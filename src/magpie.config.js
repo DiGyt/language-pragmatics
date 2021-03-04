397f766be9d0fdfabe89ca25aab28f843fcc0e46
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2021-03-04 18:00:45 +0100

    see if results are added correctly

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,6 +1,6 @@
 export default {
   experimentId: '174',
-  submissionUrl: 'http://magpie-demo.herokuapp.com/api/submit_experiment/174',
+  submissionUrl: 'https://magpie-demo.herokuapp.com/api/submit_experiment/',
   socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
   contactEmail: 'test@random.com'
 };
