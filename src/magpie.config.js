d277601f378e4c4661972ca2953867e1c8151731
Dirk Gütlin <dirk.guetlin@stud.sbg.ac.at>
2021-06-18 14:55:29 +0200

    changed heroku experiment adrress

--- src/magpie.config.js
+++ src/magpie.config.js
@@ -1,5 +1,5 @@
 export default {
-  experimentId: '174',
+  experimentId: '255',
   submissionUrl: 'https://magpie-demo.herokuapp.com/api/submit_experiment/',
   socketUrl: 'wss://magpie-demo.herokuapp.com/socket',
   contactEmail: 'test@random.com'
